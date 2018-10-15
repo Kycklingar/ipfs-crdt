5fee1567048fa4eb570a7954373ba353c1dea67d
kycklingar <apanalex@hotmail.com>
2018-10-15 19:12:56 +0200

    add search

--- javascript/crdt.js
+++ javascript/crdt.js
@@ -55,6 +55,25 @@ function Manager(channel, api="http://localhost:5001/api/v0/")
         }).bind(this))
     }).bind(this)
 
+    this.Ask = (function()
+    {
+        this.Ipfs.Publish("ASK")
+    }).bind(this)
+
+    this.Search = (function(query)
+    {
+        let tmpData = this.Object.data
+        let results = []
+        for(let i = 0; i < tmpData.length; i++)
+        {
+            if(tmpData[i].Query(query))
+            {
+                results.push(tmpData[i])
+            }
+        }
+        makeResults(results)
+    }).bind(this)
+
     this.Ipfs = new Ipfs(channel, api)
     this.Object = new Object()
 
@@ -353,7 +372,7 @@ function CRDTData()
         {
             if(this.data[i] == val)
             {
-                return tru
+                return true
             }
         }
         return false
@@ -481,6 +500,77 @@ function makePost(post)
     }
 }
 
+function makeResults(posts)
+{
+    let sr = document.getElementById("searchResults")
+    if(sr == null)
+    {
+        sr = document.createElement("div")
+        sr.id = "searchResults"
+        document.body.appendChild(sr)
+    }
+
+    removeChilds(sr)
+
+    let title = document.createElement("h3")
+    title.innerText = "Results (" + posts.length + ")"
+    
+    sr.appendChild(title)
+
+    let x = document.createElement("div")
+    x.innerText = "X"
+    x.style.position = "absolute"
+    x.style.right = 0
+    x.style.top = 0
+    x.onclick = function()
+    {
+        removeChilds(sr)
+        sr.style.display = "none"
+    }
+
+    sr.appendChild(x)
+
+    sr.style.display = null
+
+    for(let i = 0; i < posts.length; i++)
+    {
+        let post = {"Hash":posts[i].data[0], "Tags":posts[i].data.slice(1)}
+        var n = document.createElement("div")
+        n.className = "post"
+        // n.id = post.Hash
+
+        var a = document.createElement("a")
+        a.href = "/ipfs/" + post.Hash
+        a.target = "_blank"
+        var h4 = document.createElement("h4")
+        h4.innerText = post.Hash
+        a.appendChild(h4)
+        n.appendChild(a)
+
+        n.appendChild(tags(post.Tags))
+        sr.appendChild(n)
+        
+        function determine(postDiv, postData)
+        {
+            var req = new XMLHttpRequest
+            req.onreadystatechange = function(){
+                if(this.readyState == 4 && this.status == 200)
+                {
+                    var ct = this.getResponseHeader("Content-Type")
+                    if(ct.split("/")[0] == "image")
+                    {
+                        b = image("/ipfs/" + postData.Hash)
+                        postDiv.children[0].replaceChild(b, postDiv.children[0].children[0])
+                    }
+                }
+            }
+            req.open("HEAD", "/ipfs/" + postData.Hash, true)
+            req.send()
+        }
+        determine(n, post)
+    }
+}
+
 function tags(tags)
 {
     var ul = document.createElement("ul")
@@ -522,4 +612,12 @@ function submitNew()
     Manager.Publish()
 
     return false
+}
+
+function removeChilds(parent)
+{
+    while(parent.children.length > 0)
+    {
+        parent.removeChild(parent.children[0])
+    }
 }
